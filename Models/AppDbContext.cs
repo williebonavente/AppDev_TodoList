4672a8b0ab9438e3b47db84fae2d375e33746fde
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2025-04-30 21:31:37 +0800

    Setting up DbContext

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Models
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<TodoItem> ToDoItems { get; set; }
+    }
+}
---
+++ Models/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TodoList.Models
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[]args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TodoListDb;Trusted_Connection=True");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
