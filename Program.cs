807868505323996f0a45d134af361f96e4355350
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2025-04-30 22:05:19 +0800

    CRUD Done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
