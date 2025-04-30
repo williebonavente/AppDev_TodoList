20763322591f3b782f829b3e13087e049ee82b88
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2025-04-30 20:46:25 +0800

    Add TodoItemModel

---
+++ Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TodoList.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Title { get; set; }
+        public bool IsCompleted { get; set;}
+
+        [Required]
+        public required string Category { get; set; }
+
+        [Range(1, 5)]
+        public int Priority { get; set; } // 1 = highest, 5 = lowest 
+
+    }
+}
